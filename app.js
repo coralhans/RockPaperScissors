93957b5d6420c4ed11c691015a75fdc94fe0fc5d
DESKTOP-9BD30O0\korall <karl.maekungas@gmail.com>
2022-01-01 17:44:13 +0200

    Missing responsiveness

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// if player presses a weapon switch divs and also display player chosen weapon(animat in) + after a 2 second timeout animate in houses picked weapon. 
+//animate in results
+//play again = switch divs back
 
 function computerPlay(){
 
@@ -6,182 +9,209 @@ function computerPlay(){
 
     return computerWep;
 }
-function getPlayerSelection(input){
-    input = prompt("Choose your weapon (rock, paper or scissors)");
-    return input;
+const pScore = document.querySelector('.player_score');
+const cScore = document.querySelector('.computer_score');
+const buttons = document.querySelectorAll('#playbutton');
+let playerScore = 0;
+let computerScore = 0;
+const buttonDiv = document.querySelector('.buttons-container');
+const battleDiv = document.querySelector('.battle');
+const playerImage = document.querySelector('.emptyplayerwep');
+const computerImage = document.querySelector('.emptycomputerwep')
+const weapon = document.querySelector('.weapon');
+const computerweapon = document.querySelector('.computerweapon');
+const pimage = document.querySelector('.pweapon-image');
+const cimage = document.querySelector('.cweapon-image');
+const winner = document.querySelector('.win');
+const tier = document.querySelector('.tie');
+const loser = document.querySelector('.loss');
+const resultContainer = document.querySelector('.result');
+var result;
+const insertResult = document.querySelector('.playerwep');
+const replayButtons = document.querySelectorAll('#againbutton');
+const gameOver = document.querySelector('#gameover');
+
+function refresh(){
+    window.location.reload();
 }
+
+function swapDivs(){
+    buttonDiv.style.display = "none";
+    battleDiv.style.display = "flex";
+}
+function resetPage(){
+    buttonDiv.style.display = "flex";
+    battleDiv.style.display = "none";
+    winner.style.display = "none";
+    loser.style.display = "none";
+    tier.style.display = "none";
+    weapon.classList.remove('rockimage-wrapper');
+    pimage.classList.remove('rockimage');
+    weapon.classList.remove('paperimage-wrapper');
+    pimage.classList.remove('paperimage');
+    weapon.classList.remove('scissorsimage-wrapper');
+    pimage.classList.remove('scissorsimage');
+    computerweapon.classList.remove('rockimage-wrapper');
+    cimage.classList.remove('rockimage')
+    computerweapon.classList.remove('paperimage-wrapper');
+    cimage.classList.remove('paperimage')
+    computerweapon.classList.remove('scissorsimage-wrapper');
+    cimage.classList.remove('scissorsimage');
+}
+function displayPlayerWep(){
+    swapDivs();
+
+    if(userChoice === "rock"){
+        weapon.classList.add('rockimage-wrapper');
+        pimage.classList.add('rockimage');
+    }
+    else if(userChoice === "paper"){
+        weapon.classList.add('paperimage-wrapper');
+        pimage.classList.add('paperimage');
+    }
+    else if(userChoice === "scissors"){
+        weapon.classList.add('scissorsimage-wrapper');
+        pimage.classList.add('scissorsimage');
+    }
+   
+}
+function displayComputerWep(){
+    if(computerWep === "rock"){
+        computerweapon.classList.add('rockimage-wrapper');
+        cimage.classList.add('rockimage')
+    }
+    else if(computerWep === "paper"){
+        computerweapon.classList.add('paperimage-wrapper');
+        cimage.classList.add('paperimage')
+    }
+    else if(computerWep === "scissors"){
+        computerweapon.classList.add('scissorsimage-wrapper');
+        cimage.classList.add('scissorsimage');
+    }
+}
+
+function displayResult(){
+    
+    let checkResult = playRound();
+    if(checkResult === "win"){
+        winner.style.display = "block";
+        resultContainer.style.display = "flex";
+    }
+    else if(checkResult === "lose"){
+        loser.style.display = "block";
+        resultContainer.style.display = "flex";
+    }
+    else if(checkResult === "tie"){
+        tier.style.display = "block";
+        resultContainer.style.display = "flex";
+    }
+    updateScore();
+    
+}
+
+function updateScore(){
+        pScore.innerHTML = playerScore;
+        cScore.innerHTML = computerScore;
+        
+
+        if(playerScore === 5){
+            
+            winner.insertAdjacentHTML('afterbegin',`<div class="gameover"><h1>game over</h1></div>`);
+            resultContainer.style.margin = "0";
+            animItem.goToAndPlay(0,true);
+            replayButtons.forEach((button) =>{
+                button.addEventListener('click', ()=>{
+                    refresh();
+                    })
+               });
+            }
+        else if(computerScore === 5){
+            loser.insertAdjacentHTML('afterbegin',`<div class="gameover2"><h1>game over</h1></div>`);
+            resultContainer.style.margin = "0";
+            replayButtons.forEach((button) =>{
+                button.addEventListener('click', ()=>{
+                    refresh();
+            })
+        });
+    }
+}
+
+replayButtons.forEach((button) =>{
+ button.addEventListener('click', ()=>{
+     resetPage();
+ })
+});
+
+    buttons.forEach((button) =>{
+        button.addEventListener('click', (e)=>{
+            userChoice = e.target.className;
+            computerWep = computerPlay();
+            displayPlayerWep();
+            setTimeout( ()=>{
+                displayComputerWep();
+            },1500);
+            setTimeout( ()=>{
+                displayResult();
+            },3000);  
+        })
+    })
 
 function playRound(){
 
-    playerSelection = getPlayerSelection();
     
-    computerWep = computerPlay();
- 
-    var playerWep = playerSelection.toLowerCase();
-    
-    if(playerWep === "rock" && computerWep=== "scissors"){
-            let result = "win";
-            return result;
-    }
-    else if(playerWep === "scissors" && computerWep=== "rock"){
-        let result = "loss";
+    if(computerWep === userChoice){
+        result = "tie";
         return result;
     }
-    else if(playerWep === "paper" && computerWep=== "scissors"){
-            let result = "loss";
-            return result;
-    }
-    else if(playerWep === "scissors" && computerWep=== "paper"){
-            let result = "win";
-            return result;
-    }
-    else if(playerWep === "rock" && computerWep=== "paper"){
-            let result = "loss";
-            return result;
-    }
-    else if(playerWep === "paper" && computerWep=== "rock"){ 
-            let result = "win";
-            return result;
-    }
-    else if(playerWep === "paper" && computerWep=== "paper"){ 
-        let result = "tie";
-        return result;
-}
-else if(playerWep === "rock" && computerWep=== "rock"){ 
-    let result = "tie";
-    return result;
-}
-else if(playerWep === "scissors" && computerWep=== "scissors"){ 
-    let result = "tie";
-    return result;
-}
- 
-}
-
-
-function game(){
-    let playerCounter = 0;
-    let computerCounter = 0;
-
-    
-    let result = playRound();
-    if(result === "win"){
-        console.log("You WIN!");
-        playerCounter++;
-    }
-    else if(result === "loss"){
-        console.log("You LOSE!");
-        computerCounter++;
-    }
-    else if(result === "tie"){
-        console.log("It's a TIE!");
-    }
-    else{
-        console.log("Check for grammar mistakes");
-    }
-    
-     result = playRound();
-    if(result ==="win"){
-        console.log("You WIN!");
-        playerCounter++;
-    }
-    else if(result ==="loss"){
-        console.log("You LOSE!");
-        computerCounter++;
-    }
-    else if(result ==="tie"){
-        console.log("It's a TIE!");
-    }
-    else{
-        console.log("Check for grammar mistakes");
-    }
-    
-    result = playRound();
-    if(result ==="win"){
-        console.log("You WIN!");
-        playerCounter++;
-    }
-    else if(result ==="loss"){
-        console.log("You LOSE!");
-        computerCounter++;
-    }
-    else if(result ==="tie"){
-        console.log("It's a TIE!");
-    }
-    else{
-        console.log("Check for grammar mistakes");
-    }
-    
-    result = playRound();
-    if(result ==="win"){
-        console.log("You WIN!");
-        playerCounter++;
-    }
-    else if(result ==="loss"){
-        console.log("You LOSE!");
-        computerCounter++;
-    }
-    else if(result ==="tie"){
-        console.log("It's a TIE!");
-    }
-    else{
-        console.log("Check for grammar mistakes");
-    }
-    result = playRound();
-    if(result ==="win"){
-        console.log("You WIN!");
-        playerCounter++;
-    }
-    else if(result ==="loss"){
-        console.log("You LOSE!");
-        computerCounter++;
-    }
-    else if(result ==="tie"){
-        console.log("It's a TIE!");
-    }
-    else{
-        console.log("Check for grammar mistakes");
-    }
-
-    let playerWins = "You win " + playerCounter + ":" + computerCounter;
-    let computerWins = "You lose " + playerCounter + ":" + computerCounter;
-    
-    if(playerCounter > computerCounter){
-        return console.log(playerWins);
-    }
-    else if(playerCounter === computerCounter){
-        console.log("You are tied with the computer")
-    }
-    else{
-        return console.log(computerWins);
-    }
+    else if(userChoice === "rock" && computerWep=== "scissors"){
+                        result = "win";
+                        playerScore++;
+                        return result;
+            }
+    else if(userChoice === "scissors" && computerWep=== "rock"){
+                    result = "lose";
+                    computerScore++;
+                    return result;
+            }
+    else if(userChoice === "paper" && computerWep=== "scissors"){
+                result = "lose";
+                computerScore++;
+                return result;
+            }
+    else if(userChoice === "scissors" && computerWep=== "paper"){
+                    result = "win";
+                    playerScore++;
+                    return result;
+            }
+    else if(userChoice === "rock" && computerWep=== "paper"){
+                    result = "lose";
+                    computerScore++;
+                    return result;
+            }
+    else if(userChoice === "paper" && computerWep=== "rock"){ 
+                    result = "win";
+                    playerScore++;
+                    return result;
+            }
 
 }
-console.log(game());
+
+var modal = document.getElementById("myModal");
+var openModalbtn = document.getElementById("modalButton");
+var closeModalbtn = document.getElementById("close");
+
+openModalbtn.onclick = () =>{
+    modal.style.display="block";
+}
+
+closeModalbtn.onclick = () =>{
+    modal.style.display = "none";
+}
+
+window.onclick = (e) =>{
+    if(e.target == modal){
+        modal.style.display = "none";
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  console.log("You WIN! Paper beats rock!");
-// console.log("You LOSE! Paper beats rock!");
-//  console.log("You WIN! Scissors beats paper!");
-// console.log("You LOSE! Scissors beats paper!");
-// console.log("You WIN! Rock beats scissors!");
-// console.log("You LOSE! Rock beats scissors!");
